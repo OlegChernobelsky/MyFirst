d2ba004d4831a94b2f9cc939f7bcff3cb36d3c23
Oleg Chernobelsky <chy.oleg@gmail.com>
2023-08-20 05:16:49 +0300

    Initial Commit

---
+++ MyFirst/MyFirstApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyFirstApp.swift
+//  MyFirst
+//
+//  Created by Oleg Chernobelsky on 20/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct MyFirstApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
